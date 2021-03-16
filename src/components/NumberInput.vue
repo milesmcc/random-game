3916402d4b05b66243f3afece8ac4ceeb4ca8a1a
R. Miles McCain <github@sendmiles.email>
2021-03-16 00:22:18 -0400

    Changes

--- src/components/NumberInput.vue
+++ src/components/NumberInput.vue
@@ -32,7 +32,6 @@ export default {
       this.$emit("input", this.nums);
     },
     enterValue(e) {
-      console.log(e);
       let val = e.key;
       if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].indexOf(val) !== -1) {
           this.enterNum(parseInt(val));
