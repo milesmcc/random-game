506a1a7e93a9a12b214ed102a53a5dc6083ad36e
R. Miles McCain <github@sendmiles.email>
2021-03-20 01:47:07 -0400

    Document utils

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-export function decumulate(numbers, start) {
-    let intervals = [];
-    let last = start;
-    for (let number of numbers) {
-        intervals.push(number - last);
-        last = number;
-    }
-    return intervals;
-}
-
+// Calculate the distances between each pair of numbers in the given array
 export function distances(numbers) {
     let distances = [];
     let last = numbers[0];
@@ -22,12 +13,16 @@ export function distances(numbers) {
     return distances;
 }
 
-// Discrete only
-const uniformCMF = (a, b, x) => (x - a + 1) / (b - a + 1);
+// The discrete uniform CDF of `x` on the range [a, b]
+const uniformCDF = (a, b, x) => (x - a + 1) / (b - a + 1);
+
+// Sample the discrete uniform distribution on the range [a, b]
 const sampleUniform = (a, b) => (a + Math.floor(Math.random() * (b - a + 1)));
+
+// Sample the expected digit distance distribution on the range [a, b]
 const sampleDist = (a, b) => Math.abs(sampleUniform(a, b) - sampleUniform(a, b));
 
-
+// The discrete uniform PDF of `x` on the range [a, b]
 const uniformPMF = (a, b, x) => {
     if (x < 0 || x > b) {
         return 0;
@@ -35,6 +30,7 @@ const uniformPMF = (a, b, x) => {
     return (1 / (b - a + 1));
 };
 
+// The PMF of two discrete uniform random variables on the range [a, b] having a difference of `x` (signed)
 const diffPMF = (a, b, x) => {
     let sum = 0;
     for (let n = a; n <= b; n++) {
@@ -43,6 +39,7 @@ const diffPMF = (a, b, x) => {
     return sum;
 }
 
+// The PMF of the digit distance distribution on the range [a, b]; this is essentially the absolute value of `diffPMF` with some bounds checking
 export const distPMF = (a, b, x) => {
     if (x < 0 || x > b) {
         return 0;
@@ -53,7 +50,8 @@ export const distPMF = (a, b, x) => {
     return diffPMF(a, b, x) + diffPMF(a, b, -1 * x);
 }
 
-const distCMF = (a, b, x) => {
+// The CDF of the digit distance distribution on the range [a, b]
+const distCDF = (a, b, x) => {
     let sum = 0;
     for (let n = a; n <= x; n++) {
         sum += distPMF(a, b, n);
@@ -61,19 +59,16 @@ const distCMF = (a, b, x) => {
     return sum;
 };
 
-/* Function: ksStatTheshold
- * ------------------------
- * Calculate the K-S statistic value that, if used as a threshold for
- * determining fit, would only exlude true fits with probability `pVal`.
- */
-function ksStatTheshold(pVal, n, sampleFunc, cmf, min, max) {
+// Calculate the K-S statistic value that, if used as a threshold for
+// determining fit, would only exlude true fits with probability `pVal`.
+function ksStatTheshold(pVal, n, sampleFunc, cdf, min, max) {
     const N_TRIALS = 10000;
 
     // Run many simulations
     let results = [];
     for (let i = 0; i < N_TRIALS; i++) {
         let sample = Array.from({ length: n }, sampleFunc);
-        let ksStat = kolmogorovSmirnov(sample, min, max, cmf);
+        let ksStat = kolmogorovSmirnov(sample, min, max, cdf);
         results.push(ksStat);
     }
 
@@ -83,7 +78,9 @@ function ksStatTheshold(pVal, n, sampleFunc, cmf, min, max) {
     return [results[Math.floor((1 - pVal) * results.length)], results];
 }
 
-function kolmogorovSmirnov(points, min, max, cmf) {
+// Compute the Kolmogorov-Smirnov statistic for the given points on the given
+// range with the given theorized CDF. The step size is assumed to be 1.
+function kolmogorovSmirnov(points, min, max, cdf) {
     let supremum = 0;
     let sorted = points.filter(x => true).sort((a, b) => a - b);
     let dist = counts(sorted);
@@ -92,7 +89,8 @@ function kolmogorovSmirnov(points, min, max, cmf) {
     for (let x = min; x <= max; x++) {
         totalSoFar += dist[x] || 0;
         let cumulative = totalSoFar / sorted.length;
-        let diff = Math.abs(cumulative - cmf(x));
+        let diff = Math.abs(cumulative - cdf(x));
+
         if (diff > supremum) {
             supremum = diff;
         }
@@ -101,6 +99,7 @@ function kolmogorovSmirnov(points, min, max, cmf) {
     return supremum;
 }
 
+// Compute the frequency of each particular value in the `nums` array.
 function counts(nums) {
     let out = {};
     for (let num of nums) {
@@ -113,6 +112,7 @@ function counts(nums) {
     return out;
 }
 
+// The factorial function!
 function factorial(n) {
     if (n == 0) {
         return 1;
@@ -124,6 +124,8 @@ function factorial(n) {
     return x;
 }
 
+// The probability of a uniform distribution taking on
+// the values in `vals` (order is not considered).
 export function probOfUniform(min, max, vals) {
     let n = max - min + 1;
     let nCounts = counts(vals);
@@ -136,14 +138,17 @@ export function probOfUniform(min, max, vals) {
     return x * ((1 / n) ** vals.length);
 }
 
+// This is the primary entrypoint to the analysis. This function performs the K-S test
+// simulations, then determines how well the given data matches the expected distributions.
+// The `pVal` is used in determining the K-S stat theshold; see the writeup for more information.
 export function howRandom(nums, pVal) {
     let numDistances = distances(nums);
 
-    let [absThreshold, absKSSims] = ksStatTheshold(pVal, nums.length, () => sampleUniform(0, 9), x => uniformCMF(0, 9, x), 0, 9);
-    let [distThreshold, distKSSims] = ksStatTheshold(pVal, numDistances.length, () => sampleDist(0, 9), x => distCMF(0, 9, x), 0, 9);
+    let [absThreshold, absKSSims] = ksStatTheshold(pVal, nums.length, () => sampleUniform(0, 9), x => uniformCDF(0, 9, x), 0, 9);
+    let [distThreshold, distKSSims] = ksStatTheshold(pVal, numDistances.length, () => sampleDist(0, 9), x => distCDF(0, 9, x), 0, 9);
 
-    let ksAbsolute = kolmogorovSmirnov(nums, 0, 9, x => uniformCMF(0, 9, x));
-    let ksDistances = kolmogorovSmirnov(numDistances, 0, 9, x => distCMF(0, 9, x));
+    let ksAbsolute = kolmogorovSmirnov(nums, 0, 9, x => uniformCDF(0, 9, x));
+    let ksDistances = kolmogorovSmirnov(numDistances, 0, 9, x => distCDF(0, 9, x));
 
     let ksAbsoluteVerdict = ksAbsolute <= absThreshold;
     let ksDistanceVerdict = ksDistances <= distThreshold;
@@ -162,6 +167,7 @@ export function howRandom(nums, pVal) {
     }
 }
 
+// Style constants for Plotly.
 export const plotlyLayout = {
     plot_bgcolor: "#0b0f18",
     paper_bgcolor: "#0b0f18",
@@ -170,4 +176,5 @@ export const plotlyLayout = {
     legend: { font: { color: "white" } },
     title: { font: { color: "white" } }
 };
+
 /* eslint-enable no-unused-vars */
